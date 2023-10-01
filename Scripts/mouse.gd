e02ecb56f09e3c084d848d3b3e9edd51492d6661
obada-ab <obada.alabbadi.dev@gmail.com>
2023-10-01 19:48:21 +0200

    Add hadamard block

--- Scripts/mouse.gd
+++ Scripts/mouse.gd
@@ -8,6 +8,7 @@ var screen_size # Size of the game window.
 var rotating = false
 var target_angle
 var current_quarter
+var shake = 1
 	
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -19,6 +20,8 @@ func _process(delta):
 		$AnimatedSprite2D.play()
 	elif qubit == 0:
 		$AnimatedSprite2D.play_backwards()
+	else:
+		$AnimatedSprite2D.stop()
 	if rotating:
 		var current_angle = int(round($AnimatedSprite2D.rotation_degrees))
 		if qubit == 1:
@@ -30,10 +33,12 @@ func _process(delta):
 			rotating = false
 	else:
 		position += velocity * delta
-	$AnimatedSprite2D.offset = Vector2(randf(), randf())
+	$AnimatedSprite2D.offset = shake * Vector2(randf(), randf())
 
 
 func _on_area_entered(area):
+	position = (position / 50).round() * 50
+	rotating = true
 	if qubit == 1:
 		var current_angle = int(round($AnimatedSprite2D.rotation_degrees))
 		direction = direction.rotated(-PI/2)
@@ -44,8 +49,20 @@ func _on_area_entered(area):
 		direction = direction.rotated(PI/2)
 		target_angle = add_degrees(current_angle, 90)
 		current_quarter = add_degrees(current_angle, 1) / 90
-	position = (position / 50).round() * 50
-	rotating = true
+	else:
+		for i in range(2):
+			var new_mouse = duplicate()
+			new_mouse.position = position
+			new_mouse.qubit = i
+			new_mouse.modulate.a = 0.8
+			new_mouse.shake = 1
+			add_sibling(new_mouse)
+		queue_free()
 
 func add_degrees(angle1, angle2):
 	return (angle1 + angle2 + 360) % 360 
+
+
+func _on_hadamard_detector_area_entered(area):
+	qubit = 2
+	shake = 2.5
